69d92ac80d31a3d73be408ed103dd2100dd9b244
aungaungchannyein <aungaung.channyein@gmail.com>
2023-12-22 23:34:45 +0630

    second

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { db } from "./db"
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+  const renderSnippets = snippets.map((snippet)=>{
+    return <div key={snippet.id}>{snippet.title}</div>
+  })
+return <div>{renderSnippets}</div>
+}
---
+++ app/snippets/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { db } from "@/app/db";
+import { notFound } from "next/navigation";
+
+interface SnippetshowPageProps{
+  params:{
+    id: string
+  }
+}
+export default async function SnippetshowPage(props:SnippetshowPageProps){
+  await new Promise((r)=> setTimeout(r,2000));
+  
+  const snippet = await db.snippet.findFirst({
+    where:{id:parseInt(props.params.id)}
+  });
+
+  if(!snippet){
+    return notFound();
+  }
+
+  return <div>{snippet.title}</div>
+}
