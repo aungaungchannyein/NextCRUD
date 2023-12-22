09b741761f4f2fcde464cec9dc80355d75d16680
aungaungchannyein <aungaung.channyein@gmail.com>
2023-12-23 00:27:07 +0630

    third

--- app/snippets/[id]/page.tsx
+++ app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/app/db";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 interface SnippetshowPageProps{
   params:{
@@ -17,5 +18,17 @@ export default async function SnippetshowPage(props:SnippetshowPageProps){
     return notFound();
   }
 
-  return <div>{snippet.title}</div>
+  return <div>
+    <div className="flex m-4 justify-between items-center">
+      <h1 className="text-xl font-bold">{snippet.title}</h1>
+      <div className="flex gap-4">
+        <Link href={`/snippets/${snippet.id}/edit`} 
+        className="p-2 border rounded">Edit</Link>
+        <button className="p-2 border rounded">Delete</button>
+      </div>
+    </div>
+    <pre className="p-3 border rounded bg-gray-200 border-gray-200">
+      <code>{snippet.code}</code>
+    </pre>
+  </div>
 }
